1a729455faa7c5d09bfa865247c1c7517f67dc07
greencarrot5 <65950433+greencarrot5@users.noreply.github.com>
2020-05-26 15:30:35 +0200

    Initial file

--- __init__.py
+++ __init__.py
@@ -3,13 +3,20 @@ from tkinter import *
 
 #main class
 class game():
-    def __init__(self, title, bg="lightblue"):
+    def __init__(self, title, width=100, height=100, bg="lightblue"):
         self.screen = Tk()
         self.title = title
         self.bg = bg
+        self.width = width
+        self.height = height
         self.screen.title(title)
+        self.canvas = Canvas(self.screen, width=self.with, height=self.height)
+        self.canvas.pack()
     
-    def title(screentitle):
+    def title(self, screentitle):
         if screentitle:
             self.title = screentitle
             return self.screen.title(screentitle)
+    
+    def update(self):
+        self.screen.update()
